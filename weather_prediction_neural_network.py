63c9a2e51a43fa6f060155f135bcf318947de0d0
Zillennium Developer <thornsovannarath@gmail.com>
2020-11-12 13:06:21 +0700

    Update some code

--- weather_prediction_neural_network.py
+++ weather_prediction_neural_network.py
@@ -189,6 +189,7 @@ class ArtificialNeuralNetwork:
         self.output_layer.inspect()
         print('------')
 
+    # Method call to calculate feed forward with multilayer neural network
     def feed_forward(self, inputs):
         layer_feed_forward_results = [0] * self.num_layers
         for l in range(self.num_layers):
@@ -198,26 +199,30 @@ class ArtificialNeuralNetwork:
                 layer_feed_forward_results[l] = self.hidden_layer[l].feed_forward(layer_feed_forward_results[l-1])
         return self.output_layer.feed_forward(layer_feed_forward_results[len(self.hidden_layer)-1])
 
+    # Method for back propagation calculation process
     # Uses online learning, ie updating the weights after each training case
     def train(self, training_inputs, training_outputs):
         self.feed_forward(training_inputs)
 
         # 1. Output neuron deltas
+        # Calculation of ∂E/∂Netⱼ (∂E/∂yⱼ) of each node in output layer
         partial_derivative_errors_with_respect_to_output_neuron_total_net_input = [0] * len(self.output_layer.neurons)
         for o in range(len(self.output_layer.neurons)):
-            # ∂E/∂Netⱼ
             partial_derivative_errors_with_respect_to_output_neuron_total_net_input[o] = self.output_layer.neurons[o].calculate_partial_derivative_error_with_respect_to_total_net_input(training_outputs[o])
 
         # 2. Hidden neuron deltas
-        partial_derivative_errors_with_respect_to_hidden_neuron_total_net_input = [0] * len(self.hidden_layer.neurons)
-        for h in range(len(self.hidden_layer.neurons)):
-            # We need to calculate the derivative of the error with respect to the output of each hidden layer neuron
-            # dE/dyⱼ = Σ ∂E/∂Netⱼ * ∂z/∂yⱼ = Σ ∂E/∂Netⱼ * wᵢⱼ
-            derivative_error_with_respect_to_hidden_neuron_output = 0
-            for o in range(len(self.output_layer.neurons)):
-                derivative_error_with_respect_to_hidden_neuron_output += partial_derivative_errors_with_respect_to_output_neuron_total_net_input[o] * self.output_layer.neurons[o].weights[h]
-            # ∂E/∂Netⱼ = dE/dyⱼ * ∂Netⱼ/∂
-            partial_derivative_errors_with_respect_to_hidden_neuron_total_net_input[h] = derivative_error_with_respect_to_hidden_neuron_output * self.hidden_layer.neurons[h].calculate_partial_derivative_total_net_input_with_respect_to_input()
+        # Calculation of ∂E/∂Netⱼ (∂E/∂yⱼ) of each node of each hidden layer
+        partial_derivative_errors_with_respect_to_hidden_neuron_total_net_input = [0] * self.num_layers
+        for l in range(self.num_layers):
+            partial_derivative_errors_with_respect_to_hidden_neuron_total_net_input[l] = [0] * len(self.hidden_layer[l].neurons)
+            for h in range(len(self.hidden_layer[l].neurons)):
+                # We need to calculate the derivative of the error with respect to the output of each hidden layer neuron
+                # dE/dyⱼ = Σ ∂E/∂Netⱼ * ∂z/∂yⱼ = Σ ∂E/∂Netⱼ * wᵢⱼ
+                derivative_error_with_respect_to_hidden_neuron_output = 0
+                for o in range(len(self.output_layer.neurons)):
+                    derivative_error_with_respect_to_hidden_neuron_output += partial_derivative_errors_with_respect_to_output_neuron_total_net_input[o] * self.output_layer.neurons[o].weights[h]
+                # ∂E/∂Netⱼ = dE/dyⱼ * ∂Netⱼ/∂
+                partial_derivative_errors_with_respect_to_hidden_neuron_total_net_input[l][h] = derivative_error_with_respect_to_hidden_neuron_output * self.hidden_layer[l].neurons[h].calculate_partial_derivative_total_net_input_with_respect_to_input()
 
         # 3. Update output neuron weights
         for o in range(len(self.output_layer.neurons)):
@@ -228,12 +233,13 @@ class ArtificialNeuralNetwork:
                 self.output_layer.neurons[o].weights[w_ho] -= self.LEARNING_RATE * partial_derivative_error_with_respect_to_weight
 
         # 4. Update hidden neuron weights
-        for h in range(len(self.hidden_layer.neurons)):
-            for w_ih in range(len(self.hidden_layer.neurons[h].weights)):
-                # ∂Eⱼ/∂wᵢ = ∂E/∂Netⱼ * ∂zⱼ/∂wᵢ
-                partial_derivative_error_with_respect_to_weight = partial_derivative_errors_with_respect_to_hidden_neuron_total_net_input[h] * self.hidden_layer.neurons[h].calculate_partial_derivative_total_net_input_with_respect_to_weight(w_ih)
-                # Δw = α * ∂Eⱼ/∂wᵢ
-                self.hidden_layer.neurons[h].weights[w_ih] -= self.LEARNING_RATE * partial_derivative_error_with_respect_to_weight
+        for l in range(self.num_layers):
+            for h in range(len(self.hidden_layer[l].neurons)):
+                for w_ih in range(len(self.hidden_layer[l].neurons[h].weights)):
+                    # ∂Eⱼ/∂wᵢ = ∂E/∂Netⱼ * ∂zⱼ/∂wᵢ
+                    partial_derivative_error_with_respect_to_weight = partial_derivative_errors_with_respect_to_hidden_neuron_total_net_input[l][h] * self.hidden_layer[l].neurons[h].calculate_partial_derivative_total_net_input_with_respect_to_weight(w_ih)
+                    # Δw = α * ∂Eⱼ/∂wᵢ
+                    self.hidden_layer[l].neurons[h].weights[w_ih] -= self.LEARNING_RATE * partial_derivative_error_with_respect_to_weight
 
     def calculate_total_error(self, training_sets):
         total_error = 0
