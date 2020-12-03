5de16497d494028e96803b0c87b7bbf032d1eef8
Zillennium Developer <thornsovannarath@gmail.com>
2020-12-03 10:16:45 +0700

    update code

--- weather_prediction_neural_network.py
+++ weather_prediction_neural_network.py
@@ -254,6 +254,7 @@ class ArtificialNeuralNetwork:
 # ****************************************************** #
 
 
+
 # Blog post example:
 ann = ArtificialNeuralNetwork(2, 2, 2, hidden_layer_weights=[0.15, 0.2, 0.25, 0.3], hidden_layer_bias=0.35, output_layer_weights=[0.4, 0.45, 0.5, 0.55], output_layer_bias=0.6)
 for i in range(10000):
