858897c5590d14b9c0b871183a4970c8103610f3
sovannarath.thron <sovannarath.thorn@gaea-sys.com>
2021-05-28 10:17:34 +0700

    Update Code

--- weatherPrediction.py
+++ weatherPrediction.py
@@ -96,10 +96,9 @@ def trainingNeuralNetwork():
                         print(total_error) 
                         tmp_data = copy.deepcopy(weatherData[record_count])
                         tmp_data.append(total_error)
-                        tmp_data.append(freq_epoch)
+                        #tmp_data.append(freq_epoch)
                         helper.writeResultToCSV(tmp_data, 'Result_' + str(epoch) +"_epoch_"+ str(layer) + "_layer_" + str(input_ele_numbers*(int(hid_node_incre) + 1)) + "_node_" + crrentFileUpload)
                         record_count += 1
-                    
                     time.sleep(1)
                     end_time = time.time()
                     execution_time = []
@@ -122,30 +121,17 @@ def trainingNeuralNetwork():
 
 def trainingResult():
     print('\n')
+    my_table = PrettyTable()
+    my_table.field_names = ["Epoch", "Layer Number", "Number of Node", "Correct", "Incorrect", "Correct Prediction (%)"]
+    
     for epoch in [40, 80, 160, 500, 1000, 1500, 2000] :
         for layer in [1, 2]:
             for hid_node_incre in range(6):
-                weatherData = helper.readCSV('Result_' + str(epoch) + "_epoch_" + str(layer) + "_layer_" + str(6*(int(hid_node_incre) + 1)) + "_node_test.csv")
-                execute_time = weatherData[len(weatherData)-1]
-                del weatherData[len(weatherData)-1]
-                #print('Result_' + str(epoch) + "_epoch_" + str(layer) + "_layer_" + str(6*(int(hid_node_incre) + 1)) + "_node_test.csv")
-                correct_count = 0
-                incorrect_count = 0
-                for w_D in weatherData :
-                    print(float(w_D[len(w_D)-2]))
-                    if float(w_D[len(w_D)-2]) <= 0.01 :
-                        correct_count += 1
-                    else :
-                        incorrect_count += 1
-            print("Correct: " + str(correct_count) + "  Incorrect: " + str(incorrect_count))
-    """my_table = PrettyTable()
-    my_table.field_names = ["No.", "Name", "Grade", "Age"]
-    my_table.add_row([1, "Bob", 6, 11])
-    my_table.add_row([2, "Freddy", 4, 10])
-    my_table.add_row([3, "John", 7, 13])
-    my_table.sortby = 'Age'
-    my_table.reversesort = True
-    print(my_table)"""
+                total_data = helper.readingResultSet('Result_' + str(epoch) + "_epoch_" + str(layer) + "_layer_" + str(6*(int(hid_node_incre) + 1)) + "_node_test.csv")
+                my_table.add_row([epoch, layer, 6*(int(hid_node_incre) + 1), total_data[1], total_data[2], str((total_data[1]/total_data[0])*100) + '%'])
+    #my_table.sortby = 'Age'
+    #my_table.reversesort = True
+    print(my_table)
     print('\n')
 
 def exitProgram():
